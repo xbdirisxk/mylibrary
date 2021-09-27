7f93a5070ced423c8a18728bfed63dfac38a5e62
abdirisakhd <abdirisakjr@gmail.com>
2021-09-27 10:55:58 +0300

    display books in the page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let myLibrary = [
 	{ title: "dharaaro xasuustood", author: "guuleed", pages: 412, read: true },
+	{ title: "book2", author: "jama geele", pages: 112, read: false },
+	{ title: "halhays", author: "warfaa", pages: 520, read: true },
 ];
 
 function book(title, author, pages, read) {
@@ -12,16 +14,34 @@ function book(title, author, pages, read) {
 function addBookToLibrary() {
 	let book1 = new book("siinlay", "yuusuf", 243, true);
 	let book2 = new book("hawaale waran", "hadraawi", 313, false);
-	myLibrary.push(book1);
-	myLibrary.push(book2);
+	/* myLibrary.push(book1);
+	myLibrary.push(book2); */
 }
 
-addBookToLibrary();
+// addBookToLibrary();
 
 // DOM
-let container = document.querySelector(".container");
-let bookbox = document.createElement("div");
-let p = document.createElement("p");
+const container = document.querySelector(".container");
 
-container.append(bookbox);
-bookbox.append(p);
+function createNewBox() {
+	for (let key in myLibrary) {
+		let bookbox = document.createElement("div");
+		let book = myLibrary[key];
+
+		let p = document.createElement("p");
+		p.textContent = book.title;
+		bookbox.append(p);
+
+		p = document.createElement("p");
+		p.textContent = book.author;
+		bookbox.append(p);
+
+		p = document.createElement("p");
+		p.textContent = book.pages;
+		bookbox.append(p);
+
+		container.append(bookbox);
+	}
+}
+
+createNewBox();
